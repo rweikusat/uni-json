cf785fdf6477af641f51362f49905f4dd7439a53
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-21 15:40:00 +0100

    initial binding definition

--- include/uni_json_binding.h
+++ include/uni_json_binding.h
@@ -6,4 +6,28 @@
 #ifndef uni_json_binding_h
 #define uni_json_binding_h
 
+/*  includes */
+#include <stddef.h>
+
+/*  types */
+struct uni_json_binding
+{
+    /*  objects */
+    void *(*make_object)(void);
+    int (*add_2_object)(void *key, void *value, void *obj);
+
+    /*  arrays */
+    void *(make_array)(void);
+    int (*add_2_array)(void *value, void *ary);
+
+    /*  strings */
+    void *(*make_string)(void);
+    int (*add_2_string)(uint8_t *data, size_t len, void *str);
+
+    /*  simple types */
+    void *(*make_null)(void);
+    void *(*make_bool)(int true_false);
+    void *(*make_number)(uint8_t *data, size_t len);
+};
+
 #endif
