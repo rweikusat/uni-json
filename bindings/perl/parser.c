21404daaa2e3ea172247fa2a04ae84395d0653c6
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-23 13:24:19 +0100

    parse routine and make_hv

--- bindings/perl/parser.c
+++ bindings/perl/parser.c
@@ -5,3 +5,31 @@
 
   MIT-licensed
 */
+
+/*  includes */
+#define PERL_NO_GET_CONTEXT
+#include "EXTERN.h"
+#include "perl.h"
+
+#include <uni_json_p_bindings.h>
+#include <uni_json_parser.h>
+
+/*  prototypes */
+static void *make_hv(void);
+
+/*  variables */
+static struct uni_json_p_binding bind = {
+    .make_object =	make_hv
+};
+
+/*  routines */
+static void *make_hv(void)
+{
+    dTHX;
+    return newHV();
+}
+
+void *parse(uint8_t *data, size_t len)
+{
+    return uni_json_parse(data, len, &bind);
+}
