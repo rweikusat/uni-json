e08e592070a5fb6b2b4d00a8bfde920f0bdc3662
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-23 13:58:05 +0100

    boilerplate DynaLoader code from man page

--- bindings/perl/lib/JSON/Uni.pm
+++ bindings/perl/lib/JSON/Uni.pm
@@ -3,5 +3,12 @@
 # Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
 #
 
+#*  code
+#
+package JSON::Uni;
+require DynaLosder;
+our @ISA = 'DynaLoader';
+__PACKAGE__->bootstrap();
+
 # Ach ja
 1;
