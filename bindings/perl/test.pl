e6b21dd6d9f5d4cad83a2da5218be0db71f2eecd
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-27 12:46:00 +0100

    overall and literal parsing tests

--- bindings/perl/test.pl
+++ bindings/perl/test.pl
@@ -1,6 +1,59 @@
 # uni-json test script
 #
 
-use JSON::Uni 'parse_json';
+use Test::More tests => 9;
 
-my $x = parse_json('[1, 2, 3]');
+#**  overall tests
+#
+BEGIN {
+    use_ok('JSON::Uni', 'parse_json');
+}
+
+{
+    my $x = parse_json('');
+    ok(!defined($x), 'empty string parses as null');
+}
+
+eval {
+    parse_json(' ');
+};
+isnt($@, '', 'non-empty string with no value returns an error');
+
+#**  literal parsing
+#
+{
+    my $x = parse_json('null');
+    ok(!defined($x), 'null parses as undef');
+}
+
+{
+    my $x = parse_json('true');
+    ok(builtin::is_bool($x) && $x, 'parsing true returns a boolean true value');
+}
+
+{
+    my $x = parse_json('false');
+    ok(builtin::is_bool($x) && !$x, 'parsing false returns a boolean false value');
+}
+
+{
+    my $x;
+    eval {
+        $x = parse_json("\t\r\n   null\r\r\t\n");
+    };
+    ok($@ eq '' && !defined($x), 'values can have leading and trailing whitespace');
+}
+
+{
+    eval {
+        parse_json('flase');
+    };
+    isnt($@, '', 'wrong chars in literals error');
+}
+
+{
+    eval {
+        parse_json('nuller');
+    };
+    isnt($@, '', 'additional chars in literals error');
+}
