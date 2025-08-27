ee1159c5a563f88db8d45953064d907555921666
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-27 11:01:23 +0100

    top-level parser/ parse_value routine

--- src/uni_json_parser.c
+++ src/uni_json_parser.c
@@ -27,6 +27,8 @@ typedef void *parse_func(struct pstate *, struct uni_json_p_binding *);
 /*  prototypes */
 static void *whitespace(struct pstate *, struct uni_json_p_binding *);
 
+static void *parse_value(struct pstate *, struct uni_json_p_binding *);
+
 /*  variables */
 static parse_func *tok_map[256] = {
     ['\t'] =		whitespace,
@@ -36,7 +38,8 @@ static parse_func *tok_map[256] = {
 };
 
 static char *ec_msg_map[] = {
-    [UJ_E_INV] =	"invalid token start char"
+    [UJ_E_INV] =	"invalid token start char",
+    [UJ_E_NO_VAL] =	"missing value"
 };
 
 /*  routines */
@@ -45,6 +48,41 @@ static void *whitespace(struct pstate *, struct uni_json_p_binding *)
 {
     /* dummy function used to mark whitespace chars in tok_map */
     return NULL;
+}
+
+static void *parse_value(struct pstate *pstate, struct uni_json_p_binding *binds)
+{
+    parse_func *parse;
+    uint8_t *p, *e;
+    void *v;
+
+    p = pstate->p;
+    e = pstate->e;
+
+    while (p < e && (parse = tok_map[*p], parse == whitespace))
+        ++p;
+    if (p == e) {
+        pstate->err.code = UJ_E_NO_VAL;
+        pstate->err.pos = p;
+        return NULL;
+    }
+
+    if (!parse) {
+        pstate->err.code = UJ_E_INV;
+        pstate->err.pos = p;
+        return NULL;
+    }
+
+    pstate->p = p;
+    v = parse(pstate, binds);
+    if (!v) return NULL;
+
+    p = pstate->p;
+    while (p < e && (parse = tok_map[*p], parse == whitespace))
+           ++p;
+
+    pstate->p = p;
+    return v;
 }
 
 /**  interface */
@@ -57,6 +95,19 @@ char *uni_json_ec_2_msg(unsigned ec)
 
 void *uni_json_parse(uint8_t *data, size_t len, struct uni_json_p_binding *binds)
 {
-    fputs("I won't parse!\n", stderr);
-    return NULL;
+    struct pstate pstate;
+    void *v;
+
+    if (!len) return NULL;
+
+    pstate.s = pstate.p = data;
+    pstate.e = data + len;
+
+    v = parse_value(&pstate, binds);
+    if (!v) {
+        binds->on_error(pstate.err.code, pstate.err.pos - data);
+        return NULL;
+    }
+
+    return v;
 }
