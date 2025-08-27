2eae9e035af093c200251913d01f6f90666e0b4a
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-27 11:50:59 +0100

    make_bool

--- bindings/perl/parser.c
+++ bindings/perl/parser.c
@@ -17,6 +17,7 @@
 /*  prototypes */
 static void on_error(unsigned, size_t);
 
+static void *make_bool(int);
 static void *make_hv(void);
 static void *make_null(void);
 
@@ -25,6 +26,8 @@ static struct uni_json_p_binding binding = {
     .on_error =		on_error,
 
     .make_object =	make_hv,
+
+    .make_bool =	make_bool,
     .make_null =	make_null
 };
 
@@ -44,6 +47,12 @@ static void *make_null(void)
 {
     dTHX;
     return &PL_sv_undef;
+}
+
+static void *make_bool(int true_false)
+{
+    dTHX;
+    return true_false ? &PL_sv_yes : &PL_sv_no;
 }
 
 void *parse(uint8_t *data, size_t len)
