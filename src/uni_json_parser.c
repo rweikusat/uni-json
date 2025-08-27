cbdcbc0c3d4aacaa315bcf9e60f3e4eeb9096142
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-27 10:20:01 +0100

    struct pstate/ parse_func typedef

--- src/uni_json_parser.c
+++ src/uni_json_parser.c
@@ -12,6 +12,18 @@
 #include "uni_json_p_binding.h"
 #include "uni_json_parser.h"
 
+/*  types */
+struct pstate {
+    uint8_t *s, *p, *e;
+
+    struct {
+        unsigned code;
+        uint8_t *pos;
+    } err;
+};
+
+typedef void *parse_func(struct pstate *);
+
 /*  routines */
 void *uni_json_parse(uint8_t *data, size_t len, struct uni_json_p_binding *bind)
 {
