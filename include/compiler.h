e7cac138a3dd8aa0bd3c766d62f4d9cf6999c96c
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-02 12:39:56 +0100

    compiler header defining _hidden_ macro

---
+++ include/compiler.h
@@ -0,0 +1,13 @@
+/*
+  convenience macros for gcc features
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed.
+*/
+#ifndef uni_json_compiler_h
+#define uni_json_compiler_h
+
+#define _hidden_ __attribute__ ((visibility ("hidden")))
+
+#endif
