40fa57bee19a7b5b31d8c8331bb8fece959a65fe
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-02 13:23:34 +0100

    declare structure types

--- src/lib.h
+++ src/lib.h
@@ -11,6 +11,10 @@
 /*  includes */
 #include "compiler.h"
 
+/*  types */
+struct pstate;
+struct uni_json_p_binding;
+
 /*  routines */
 void free_obj(int type, void *obj, struct uni_json_p_binding *binds) _hidden_;
 int have_one_of(struct pstate *pstate, uint8_t *set) _hidden_;
