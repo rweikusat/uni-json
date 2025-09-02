68f06f23516041f24cba477e6b4984a612f4f28e
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-02 13:22:32 +0100

    utility functions header

---
+++ src/lib.h
@@ -0,0 +1,18 @@
+/*
+  utility functions
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed.
+*/
+#ifndef uni_json_lib_h
+#define uni_json_lib_h
+
+/*  includes */
+#include "compiler.h"
+
+/*  routines */
+void free_obj(int type, void *obj, struct uni_json_p_binding *binds) _hidden_;
+int have_one_of(struct pstate *pstate, uint8_t *set) _hidden_;
+
+#endif
