67eabb28029167ac1b5a26e2ca76932dbae16356
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-03 12:17:33 +0100

    turned parse_array_content into parse_object_content

--- src/parser_object.c
+++ src/parser_object.c
@@ -18,22 +18,50 @@ extern int no_value;
 void *parse_value(struct pstate *, struct uni_json_p_binding *);
 
 /*  routines */
-static int parse_array_content(struct pstate *pstate, struct uni_json_p_binding *binds,
-                               void *ary)
+static int parse_object_content(struct pstate *pstate, struct uni_json_p_binding *binds,
+                                void *obj)
 {
-    void *v;
+    void *k, *v;
+    uint8_t *pos;
     int c, rc;
 
-    v = parse_value(pstate, binds);
-    if (!v) return -1;
+    pos = pstate->p;
+    k = parse_value(pstate, binds);
+    if (!k) return -1;
+    if (pstate->last_type != T_STR) {
+        free_obj(pstate->last_type, k, binds);
+
+        pstate->err.code = UJ_E_INV_KEY;
+        pstate->err.pos = pos;
+        return -1;
+    }
 
     if ((int *)v == &no_value) {
-        c = have_one_of(pstate, "]");
+        c = have_one_of(pstate, "}");
         if (c == -1) return -1;
     } else
         do {
-            rc = binds->add_2_array(v, ary);
+            c = have_one_of(pstate, ':');
+            if (c == -1) {
+                binds->free_string(key);
+                return -1;
+            }
+
+            v = parse_value(binds, pstate);
+            if (!v || (iunt *)v == &no_value) {
+                binds->free_string(k);
+
+                if ((int *)v == &no_value) {
+                    pstate->err.code = UJ_E_NO_VAL;
+                    pstate->err.pos = pstate->p;
+                }
+
+                return -1;
+            }
+
+            rc = binds->add_2_object(k, v, obj);
             if (!rc) {
+                binds->free_string(k);
                 free_obj(pstate->last_type, v, binds);
 
                 pstate->err.code = UJ_E_ADD;
@@ -41,16 +69,25 @@ static int parse_array_content(struct pstate *pstate, struct uni_json_p_binding 
                 return -1;
             }
 
-            c = have_one_of(pstate, ",]");
+            c = have_one_of(pstate, ",}");
             if (c == -1) return -1;
 
             if (c == ',') {
-                v = parse_value(pstate, binds);
-                if (!v) return -1;
+                pos = pstate->p;
+                k = parse_value(pstate, binds);
+                if (!k) return -1;
 
-                if ((int *)v == &no_value) {
-                    pstate->err.code = UJ_E_NO_VAL;
+                if ((int *)k == &no_value) {
+                    pstate->err.code = UJ_E_NO_KEY;
                     pstate->err.pos = pstate->p;
+                    return -1;
+                }
+
+                if (pstate->last_type != T_STR) {
+                    free_obj(pstate->last_type, k, binds);
+
+                    pstate->err.code = UJ_E_INV_KEY;
+                    pstate->err.pos = pos;
                     return -1;
                 }
             }
