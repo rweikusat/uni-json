4e1c734b238d357ee7b4718cd3ed19c0d8354573
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-03 13:23:22 +0100

    export nesting control routines

--- bindings/perl/lib/JSON/Uni.pm
+++ bindings/perl/lib/JSON/Uni.pm
@@ -14,7 +14,7 @@ our @ISA = 'DynaLoader';
 __PACKAGE__->bootstrap();
 
 use Exporter	'import';
-our @EXPORT_OK = 'parse_json';
+our @EXPORT_OK = qw(parse_json max_nesting set_max_nesting);
 
 # Ach ja
 1;
