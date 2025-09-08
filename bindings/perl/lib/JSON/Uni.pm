c20d29ae59043f90f253c289aa04681003a855aa
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-08 13:57:49 +0100

    beginnings of embedded module documentation

--- bindings/perl/lib/JSON/Uni.pm
+++ bindings/perl/lib/JSON/Uni.pm
@@ -18,3 +18,23 @@ our @EXPORT_OK = qw(parse_json max_nesting set_max_nesting);
 
 # Ach ja
 1;
+
+__END__
+
+=head1 NAME
+
+JSON::Uni - Perl interface to uni-json parser
+
+=head1 SYNOPSIS
+
+ use JSON::Uni	qw(parse_json max_nesting set_max_nesting);
+
+ my $obj = parse_json('<JSON string>');
+ my $nesting = max_nesting();
+ set_max_nesting(<max nesting level);
+
+=head1 DESCRIPTION
+
+Something will be described here.
+
+=cut
