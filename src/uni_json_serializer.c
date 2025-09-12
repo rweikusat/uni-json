50e28a54216a30be44c180fb9822ebad67919b63
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-12 17:57:52 +0100

    dummy serializer implementation

---
+++ src/uni_json_serializer.c
@@ -0,0 +1,20 @@
+/*
+  serializer
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed.
+*/
+
+/*  includes */
+#include <stdio.h>
+
+#include "uni_json_types.h"
+#include "uni_json_s_binding.h
+#include "uni_json_serializer.h"
+
+/*  routines */
+void uni_json_serialize(void *val, void *sink, struct uni_json_s_binding *bind)
+{
+    puts("Thinkste!");
+}
