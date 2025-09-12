f09856f15f2d301b5197a5a38e97a1e59e08534e
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-12 17:59:37 +0100

    missing "/ use correct signature

--- src/uni_json_serializer.c
+++ src/uni_json_serializer.c
@@ -10,11 +10,12 @@
 #include <stdio.h>
 
 #include "uni_json_types.h"
-#include "uni_json_s_binding.h
+#include "uni_json_s_binding.h"
 #include "uni_json_serializer.h"
 
 /*  routines */
-void uni_json_serialize(void *val, void *sink, struct uni_json_s_binding *bind)
+void uni_json_serialize(void *val, void *sink, struct uni_json_s_binding *bind,
+                        int fmt)
 {
     puts("Thinkste!");
 }
