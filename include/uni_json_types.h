2bc17c035d7c51bd453507b7bda73492f5f72dcf
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-12 16:31:33 +0100

    header defining type constants

---
+++ include/uni_json_types.h
@@ -0,0 +1,20 @@
+/*
+  type constants
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed.
+*/
+#ifndef uni_json_types_h
+#define uni_json_types_h
+
+/*  constants */
+enum {
+    UJ_T_NULL,
+    UJ_T_BOOL,
+    UJ_T_NUM,
+    UJ_T_ARY,
+    UJ_T_OBJ
+};
+
+#endif
