e6be223d7cbf9ba046da0dc63c1e39623e70ea29
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-14 14:44:12 +0100

    basic serialization infrastructure and serializers for null and bool

--- src/uni_json_serializer.c
+++ src/uni_json_serializer.c
@@ -13,9 +13,58 @@
 #include "uni_json_s_binding.h"
 #include "uni_json_serializer.h"
 
+/*  types */
+typedef void serialize_func(void *val, void *sink, struct uni_json_s_binding *binds,
+                            unsigned level, int fmt);
+
+/*  prototypes */
+static void ser_null(void *, void *, struct uni_json_s_binding *,
+                     unsigned, int);
+
+static void ser_bool(void *, void *, struct uni_json_s_binding *,
+                     unsigned, int);
+
+/*  variables */
+static serialize_func *serers[] = {
+    [UJ_T_NULL] =	ser_null,
+    [UJ_T_BOOL] =	ser_bool,
+    [UJ_T_UNK] =	ser_null
+};
+
 /*  routines */
+static void ser_null(void *, void *sink, struct uni_json_s_binding *binds,
+                     unsigned, int)
+{
+    binds->output("null", 4, sink);
+}
+
+static void ser_bool(void *val, void *sink, struc uni_json_s_binding *binds,
+                     unsigned, int)
+{
+    char *vs;
+    size_t len;
+    int v;
+
+    v = binds->get_bool_value(val);
+    if (v) {
+        vs = "true";
+        len = 4;
+    } else {
+        vs = "false";
+        len = 5;
+    }
+
+    binds->output(vs, len, sink);
+}
+
+static void serialize_value(void *val, void *sink, struct uni_json_s_binding *binds,
+                            unsigned level, int fmt)
+{
+    sersers[binds->type_of(val)](val, sink, binds, level, fmt);
+}
+
 void uni_json_serialize(void *val, void *sink, struct uni_json_s_binding *binds,
                         int fmt)
 {
-    binds->output("Thinkste!", 9, sink);
+    serialize_value(val, sink, binds, 0, fmt);
 }
