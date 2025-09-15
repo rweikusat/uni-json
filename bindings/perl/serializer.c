b96a2fb2ef4c9232bb22dd363bcc7291654af923
Rainer Weikusat <rweikusat@talktalk.net>
2025-09-15 10:58:33 +0100

    array binding functions

--- bindings/perl/serializer.c
+++ bindings/perl/serializer.c
@@ -20,9 +20,20 @@ enum {
     INIT_BUF_SIZE = 128
 };
 
+/*  types */
+struct aiter {
+    SV **p, **e;
+};
+
 /*  prototypes */
 static void output(uint8_t *data, size_t len, void *sink);
 static int type_of(void *p);
+
+static void *start_array_traversal(void *);
+static void *next_value(void *);
+static void end_array_traversal(void *);
+
+
 static void get_num_data(void *num, struct uj_data *data);
 static void get_string_data(void *str, struct uj_data *data);
 static int get_bool_value(void *boolean);
@@ -31,6 +42,11 @@ static int get_bool_value(void *boolean);
 static struct uni_json_s_binding binds = {
     .output =			output,
     .type_of =			type_of,
+
+    .start_array_traversal =	start_array_traversal,
+    .next_value =		next_value,
+    .end_array_traversal =	end_array_traversal,
+
     .get_num_data =		get_num_data,
     .get_string_data =		get_string_data,
     .get_bool_value =		get_bool_value
@@ -68,6 +84,40 @@ static int type_of(void *p)
     if (SvPOK(sv)) return UJ_T_STR;
 
     return UJ_T_UNK;
+}
+
+static void *start_array_traversal(void *ary)
+{
+    dTHX;
+    struct aiter *aiter;
+    AV *av;
+
+    av = (AV *)SvRV((SV *)ary);
+
+    aiter = safemalloc(sizeof(*aiter));
+    aiter->p = AvARRAY(av);
+    aiter->e = aiter->p + av_count(av);
+
+    return aiter;
+}
+
+static void *next_value(void *p)
+{
+    dTHX;
+    struct aiter *aiter;
+    void *v;
+
+    aiter = p;
+    if (aiter->p == aiter->e) return NULL;
+
+    v = *aiter->p++;
+    return v ? v : &PL_sv_undef;
+}
+
+static void end_array_traversal(void *aiter)
+{
+    dTHX;
+    Safefree(aiter);
 }
 
 static void get_num_data(void *num, struct uj_data *data)
