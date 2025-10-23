753b57eaeb7bf11ac3eadc616c982efb0712aeda
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-23 12:41:42 +0100

    method table

--- bindings/python/parser.c
+++ bindings/python/parser.c
@@ -10,6 +10,15 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+/*  prototypes */
+static PyObject *parse_json(PyObject *, PyObject *);
+
+/*  variables */
+static PyMethodDef meths[] = {
+    {"parse_json", parse_json, METH_VARGS, "Parse a JSON string."},
+    {0}
+};
+
 /*  routines */
 static PyObject *parse_json(PyObject *, PyObject *args)
 {
