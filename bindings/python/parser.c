941acc402ae686514c86b62536447b91a28e2723
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-23 12:39:11 +0100

    first dummy function

--- bindings/python/parser.c
+++ bindings/python/parser.c
@@ -5,3 +5,20 @@
 
   MIT-licensed
 */
+
+/*  includes */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+/*  routines */
+static PyObject *parse_json(PyObject *, PyObject *args)
+{
+    uint8_t *data;
+    Py_ssize_t len;
+    int rc;
+
+    rc = PyArg_ParseTuple(args, "s#", &data, &len);
+    if (rc) fprintf(stderr, "%.*s\n", (int)len, data);
+
+    Py_RETURN_NONE;
+}
