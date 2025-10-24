c67fb0245f8895e7060ddecf0ea480fee4fe3cd5
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-24 12:55:04 +0100

    define version in setup.py/ moved parser.c to src directory

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -6,5 +6,6 @@ setup(
     author_email = "rweikusat@talktalk.net",
     description = "Interface to uni-json JSON parser/ serialize",
     license = "MIT",
-    ext_modules = [Extension('UniJson', ['parser.c'])]
+    ext_modules = [Extension('UniJson', ['src/parser.c'])],
+    version = "0.1"
 )
