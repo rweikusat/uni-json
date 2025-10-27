3791073f6ede4b69c53ae2d81ee3b0895117f3fe
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-27 14:43:59 +0000

    test_null

---
+++ bindings/python/t/parser_test.py
@@ -0,0 +1,18 @@
+#
+# test python parser bindings
+#
+
+import UniJson as uj
+import unittest
+
+class TestParserBindings(unittest.TestCase):
+    def test_error(self):
+        with self.assertRaises(ValueError, msg = 'Error handler raises ValueError'):
+            uj.parse_json('')
+
+    def test_null(self):
+        x = uj.parse_json('null')
+        self.assertIsNone(x)
+
+if __name__ == '__main__':
+    unittest.main()
