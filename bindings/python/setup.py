20d28ebd2a6fd6a528e2a65c5fe0f86e3634bd94
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-27 14:19:04 +0000

    information for linking with uni-json library

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -13,6 +13,8 @@ setup(
     #*  build info
     #
     ext_modules = [Extension('UniJson', ['src/parser.c'],
-                             include_dirs = ['../../include']
+                             include_dirs = ['../../include'],
+                             library_dirs = ['../../bin'],
+                             libraries = ['uni-json']
                              )]
 )
