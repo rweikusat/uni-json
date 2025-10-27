dc222c99d5f7b278572406a31e868e2c68fa92d5
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-27 13:48:29 +0000

    first minimal test script: test default error handler

--- bindings/python/t/parser_test.py
+++ bindings/python/t/parser_test.py
@@ -1,3 +1,14 @@
 #
 # test python parser bindings
 #
+
+import UniJson as uj
+import unittest
+
+class TestParserBindings(unittest.TestCase):
+    def test_error(self):
+        with self.assertRaises(ValueError):
+            uj.parse_json('')
+
+if __name__ == '__main__':
+    unittest.main()
