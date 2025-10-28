4847d875f4a4d13e3fc6fe8f7d9b37ccdbe52f19
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-28 17:28:48 +0000

    use size_t as len type for finalize

--- include/uni_json_p_binding.h
+++ include/uni_json_p_binding.h
@@ -38,7 +38,7 @@ struct uni_json_p_binding {
     void *(*make_string)(void);
     void (*free_string)(void *str);
     int (*add_2_string)(uint8_t *data, size_t len, void *str);
-    void *(*finalize_string)(void *str, uint8_t *data, ssize_t len);
+    void *(*finalize_string)(void *str, uint8_t *data, size_t len);
     void (*free_work_string)(void *str);
 
     /*  simple types */
