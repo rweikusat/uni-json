09f4b70f85bd6f78677a701e170e59a0759d471a
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-28 12:13:51 +0000

    added free_work_string function pointer

--- include/uni_json_p_binding.h
+++ include/uni_json_p_binding.h
@@ -39,6 +39,7 @@ struct uni_json_p_binding {
     void (*free_string)(void *str);
     int (*add_2_string)(uint8_t *data, size_t len, void *str);
     void *(*finalize_string)(void *str);
+    void (*free_work_string)(void *str);
 
     /*  simple types */
     void *(*make_null)(void);
