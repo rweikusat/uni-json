116bea0f3b1a8e24c72c238d2f13f121adf83fce
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-29 17:28:14 +0000

    test object parsing

---
+++ bindings/python/src/module.c
@@ -0,0 +1,35 @@
+/*
+  uni-json bindings for Python -- "module meta"
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed
+*/
+
+/*  includes */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+/*  prototypes */
+PyObject *parse_json(PyObject *, PyObject *);
+
+/*  variables */
+PyDoc_STRVAR(mod_doc, "uni-json JSON parser/ serializer");
+
+static PyMethodDef meths[] = {
+    {"parse_json", parse_json, METH_VARARGS, "Parse a JSON string."},
+    {0}
+};
+
+static PyModuleDef module = {
+    .m_base =		PyModuleDef_HEAD_INIT,
+    .m_name =		"UniJson",
+    .m_doc =		mod_doc,
+    .m_methods =	meths,
+};
+
+/*  routines */
+PyMODINIT_FUNC PyInit_UniJson(void)
+{
+    return PyModule_Create(&module);
+}
--- bindings/python/t/test_parser.py
+++ bindings/python/t/test_parser.py
@@ -42,5 +42,9 @@ class TestParserBindings(unittest.TestCase):
         x = uj.parse_json('[1, 2, [1, 2]]')
         self.assertEqual(x, [1, 2, [1, 2]])
 
+    def test_object(self):
+        x = uj.parse_json('{"a" : 1, "b" : 2 }')
+        self.assertEqual(x, {'a' : 1, 'b' : 2})
+
 if __name__ == '__main__':
     unittest.main()
