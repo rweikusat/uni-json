3e3f8b4f01c81696c8d0893ee91e514b0c37ec60
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-29 18:56:44 +0000

    work string header

--- bindings/python/src/work_string.h
+++ bindings/python/src/work_string.h
@@ -1,4 +1,24 @@
+/*
+   uni-json bindings for Python -- work_string
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed
+*/
 #ifndef uj_python_work_string_h
 #define uj_python_work_string_h
+
+/*  includes */
+#include <inttypes.h>
+
+/*  types */
+struct work_string {
+    uint8_t *s, *p, *e;
+};
+
+/*  routines */
+void *make_work_string(void);
+void free_work_string(void *ws);
+int add_2_work_string(uint8_t *data, size_t len, void *ws);
 
 #endif
