1195ed8447f9a9511212108ea632a8ff2d2d7297
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-29 18:27:35 +0000

    first dummy implementation of serializer entry point

---
+++ bindings/python/src/serializer.c
@@ -0,0 +1,22 @@
+/*
+  uni-json bindings for Python -- serializer
+
+  Copyright (C) 2025 Rainer Weikusat, rweikusat@talktalk.net
+
+  MIT-licensed
+*/
+
+/*  includes */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include "compiler.h"
+#include "uni_json_s_binding.h"
+#include "uni_json_serializer.h"
+
+/*  routines */
+PyObject * _hidden_ json_serialize(PyObject *, PyObject *args)
+{
+    fputs("Ha!\n", stderr);
+    Py_RETURN_NONE;
+}
