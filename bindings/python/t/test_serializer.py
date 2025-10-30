53071d78889494ffb7b0be922ed9825265555827
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-30 12:55:55 +0000

    use None as first argument for argument tests

--- bindings/python/t/test_serializer.py
+++ bindings/python/t/test_serializer.py
@@ -10,5 +10,19 @@ class TestSerializerBindings(unittest.TestCase):
         with self.assertRaises(TypeError):
             uj.json_serialize()
 
+    def test_one_arg(self):
+        uj.json_serialize(None)
+
+    def test_two_args(self):
+        uj.json_serialize(None, 0)
+
+    def test_2nd_wrong(self):
+        with self. assertRaises(ValueError):
+            uj.json_serialize(None, 17)
+
+    def test_null(self):
+        s = uj.json_serialize(None)
+        self.assertEqual(s, 'null')
+
 if __name__ == '__main__':
     unittest.main()
