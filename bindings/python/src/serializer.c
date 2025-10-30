a4d37e60ef8c06ed965f120f9d44e337ae4f835f
Rainer Weikusat <rweikusat@talktalk.net>
2025-10-30 12:27:41 +0000

    beginnings of type_of callback

--- bindings/python/src/serializer.c
+++ bindings/python/src/serializer.c
@@ -13,20 +13,35 @@
 #include "compiler.h"
 #include "uni_json_s_binding.h"
 #include "uni_json_serializer.h"
+#include "uni_json_types.h"
 #include "work_string.h"
 
 /*  prototypes */
 static void output(uint8_t *, size_t, void *);
+static int type_of(void *);
 
 /*  variables */
 static struct uni_json_s_binding binds = {
-    .output =		output
+    .output =		output,
+    .type_of =		type_of
 };
 
 /*  routines */
 static void output(uint8_t *data, size_t len, void *sink)
 {
     add_2_work_string(data, len, sink);
+}
+
+static int type_of(void *obj)
+{
+    PyTypeObject *tp;
+
+    if ((PyObject *)obj == Py_None) return UJ_T_NULL;
+
+    tp = Py_TYPE(obj);
+    if (tp == &PyBool_Type) return UJ_T_BOOL;
+
+    return UJ_T_UNK;
 }
 
 PyObject _hidden_ * json_serialize(PyObject *, PyObject *args)
