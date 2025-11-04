7a91bdd82c56bb04ece051891a829de7279210d4
Rainer Weikusat <rweikusat@talktalk.net>
2025-11-04 12:42:18 +0000

    dictionary test cases for extended handling of non-string keys

--- bindings/python/t/test_serializer.py
+++ bindings/python/t/test_serializer.py
@@ -37,14 +37,6 @@ class TestSerializerBindings(unittest.TestCase):
         s = uj.json_serialize([1, 2, ['affe']])
         self.assertEqual(s, '[1,2,["affe"]]')
 
-    def test_string_key_dict(self):
-        s = uj.json_serialize({ "b" : 1, "a" : 7 }, 1)
-        self.assertEqual(s, '{"a":7,"b":1}')
-
-    def test_non_string_key_dict(self):
-        s = uj.json_serialize({ (1,2) : 3, 4711 : "Emil", None : 'bla' }, 1)
-        self.assertEqual(s, '{"(1, 2)":3,"4711":"Emil","None":"bla"}')
-
     def test_unk_null(self):
         s = uj.json_serialize([1, (1,2), 3])
         self.assertEqual(s, '[1,null,3]')
@@ -52,6 +44,26 @@ class TestSerializerBindings(unittest.TestCase):
     def test_unk_err(self):
         with self.assertRaises(ValueError):
             uj.json_serialize([1, (1,2), 3], 4)
+
+    def test_string_key_dict(self):
+        x = uj.json_serialize({ "a" : 1, "b" : 2 })
+        self.assertTrue(x == '{"b":1,"a":2}' or x == '{"a":1,"b":2}')
+
+    def test_nsk_dict_skip_some(self):
+        x = uj.json_serialize({ 12 : 14, "emil" : "jannings", (1,2) : 11, "johnny" : "potato"}, 1)
+        self.assertEqual(x, '{"emil":"jannings","johnny":"potato"}')
+
+    def test_nsk_dict_skip_all(self):
+        x = uj.json_serialize({ 12 : 14, (1,2) : 11}, 1)
+        self.assertEqual(x, '{}')
+
+    def test_nsk_dict_err(self):
+        with self.assertRaises(ValueError):
+            uj.json_serialize({ "a" : 3, (8,9) : 1 }, 8)
+
+    def test_nsk_dict_str(self):
+        x = uj.json_serialize({"a" : "b", (8,9) : 12, 13: "alpha"}, 17)
+        self.assertEqual(x, '{"(8, 9)":12,"13":"alpha","a":"b"}')
 
 if __name__ == '__main__':
     unittest.main()
