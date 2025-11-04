bc23fdd391d2ddb5c20619a33b0081ac616f2799
Rainer Weikusat <rweikusat@talktalk.net>
2025-11-04 14:16:40 +0000

    removed , from pretty

--- bindings/python/t/test_serializer.py
+++ bindings/python/t/test_serializer.py
@@ -9,7 +9,7 @@ from enum import IntFlag
 class SerModes(IntFlag):
     UJ_FMT_FAST = 0
     UJ_FMT_DET = 1
-    UJ_FMT_PRETTY = 2,
+    UJ_FMT_PRETTY = 2
     PY_UJ_UNK_ERR = 4
     PY_UJ_NSK_ERR = 8
     PY_UJ_NSK_STR = 16
