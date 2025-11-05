f2c243687900a75a93d18c0ddf40d8041170630a
Rainer Weikusat <rweikusat@talktalk.net>
2025-11-05 11:38:39 +0000

    UJDM.py no longer needed

--- bindings/python/src/module.c
+++ bindings/python/src/module.c
@@ -10,9 +10,13 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include "uni_json_serializer.h"
+
 /*  prototypes */
 PyObject *parse_json(PyObject *, PyObject *);
 PyObject *json_serialize(PyObject *, PyObject *);
+
+static int add_consts(PyObject *);
 
 /*  variables */
 PyDoc_STRVAR(mod_doc, "uni-json JSON parser/ serializer");
@@ -23,15 +27,27 @@ static PyMethodDef meths[] = {
     {0}
 };
 
+static PyModuleDef_Slot slots[] = {
+    {Py_mod_exec, add_consts},
+    {0, NULL}
+};
+
 static PyModuleDef module = {
     .m_base =		PyModuleDef_HEAD_INIT,
     .m_name =		"UniJson",
     .m_doc =		mod_doc,
     .m_methods =	meths,
+    .m_slots =		slots
 };
 
 /*  routines */
+static int add_consts(PyObject *module)
+{
+    PyModule_AddObject(module, "UJ_FMT_FAST", PyLong_FromLong(UJ_FMT_FAST));
+    return 0;
+}
+
 PyMODINIT_FUNC PyInit_UniJson(void)
 {
-    return PyModule_Create(&module);
+    return PyModuleDef_Init(&module);
 }
